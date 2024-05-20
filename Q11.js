1c1662ff290936a319ba2ad085b5cd506e788f83
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-20 09:50:01 -0700

    Add files via upload

---
+++ Q11.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fruits = ["Apple", "banana", "pineapple", "orange"];
+// let citrusfruits=fruits. splice (0,2);
+// console.log(citrusfruits);
+fruits.splice(0, 2);
+console.log(fruits);
---
+++ Q11.ts
@@ -0,0 +1,5 @@
+let fruits=[ "Apple", "banana", "pineapple", "orange"]
+// let citrusfruits=fruits. splice (0,2);
+// console.log(citrusfruits);
+fruits.splice(0,2);
+console.log(fruits);
